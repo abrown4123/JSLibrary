28d7dd780f80122bd3837ba7b87cb86104bfbe2f
Anthony Brown <abrown4123@gmail.com>
2020-02-27 07:49:35 -0600

    Create global reference for greetr reference, which returns a Greetr obj when invoked.

--- greetr.js
+++ greetr.js
@@ -1,4 +1,19 @@
 (function(global, $){
+    let Greetr = function (firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
 
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language) {
+        let self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
 
 })(window, jQuery);
