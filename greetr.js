8aad394e25988489472a8f92b9e044fc66f48ca3
Anthony Brown <abrown4123@gmail.com>
2020-02-28 07:55:55 -0600

    Add properties to the Greetr prototype

--- greetr.js
+++ greetr.js
@@ -3,7 +3,42 @@
         return new Greetr.init(firstName, lastName, language);
     }
 
-    Greetr.prototype = {};
+    let supportedLangs = ['en', 'es'];
+
+    let greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    let formalGreeting = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    let logMessages = {
+        en: 'En logged in',
+        es: 'Es logged in'
+    };
+
+    Greetr.prototype = {
+        fullname: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid langauge";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullname();
+        }
+    };
 
     Greetr.init = function(firstName, lastName, language) {
         let self = this;
